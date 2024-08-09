4a654c49f5d0056a8cb1e405e3c5058b64d52555
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-08-09 11:55:32 +0300

    Your descriptive commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./style.css";
 
-function App() {
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            {...register("name", { required: "Name is required." })}
+          />
+          {errors.name && <p className="errorMsg">{errors.name.message}</p>}
+        </div>
+        <div className="form-control">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            {...register("email", {
+              required: "Email is required.",
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: "Email is not valid."
+              }
+            })}
+          />
+          {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+        </div>
+        <div className="form-control">
+          <label>Message</label>
+          <textarea
+            name="message"
+            {...register("message", {
+              required: "Message is required.",
+              minLength: {
+                value: 10,
+                message: "Message should be at least 10 characters long."
+              }
+            })}
+          />
+          {errors.message && <p className="errorMsg">{errors.message.message}</p>}
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
-
-export default App;
